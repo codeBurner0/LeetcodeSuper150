edd4bf39772cfe696c806104007ff1a4c998b7d5
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-06-06 22:27:37 +0530

    Time: 2 ms (58.69%), Space: 40.5 MB (72.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,23 +1,38 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if(s.length()==0){
-            return true;
-        }
-        int res=0;
-        int count=0;
-        for(int j=0;j<s.length();j++){
-            for(int i=res;i<t.length();i++){
-            if(t.charAt(i)==s.charAt(j)){
-                count++;
-                res=i+1;
-                if(count==s.length()){
-                return true;
-                }
-                break;
-            }
+    // public boolean isSubsequence(String s, String t) {
+    //     if(s.length()==0){
+    //         return true;
+    //     }
+    //     int res=0;
+    //     int count=0;
+    //     for(int j=0;j<s.length();j++){
+    //         for(int i=res;i<t.length();i++){
+    //         if(t.charAt(i)==s.charAt(j)){
+    //             count++;
+    //             res=i+1;
+    //             if(count==s.length()){
+    //             return true;
+    //             }
+    //             break;
+    //         }
             
+    //         }
+    //     }
+    //     return false;
+    // }
+    public boolean isSubsequence(String s, String t) {
+        int start1=0;
+        int start2=0;
+        int count=0;
+        while(start1<s.length() && start2<t.length()){
+            if(s.charAt(start1)==t.charAt(start2)){
+                start1++;
+                start2++;
+                count++;
+            }else{
+                start2++;
             }
         }
-        return false;
+        return count==s.length();
     }
 }
