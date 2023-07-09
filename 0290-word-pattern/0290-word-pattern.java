f4692fefd58a9e75ff5df2e1bcb486a3ebe00451
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-07-09 19:41:21 +0530

    Time: 6 ms (5.14%), Space: 40.9 MB (7.9%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,23 +1,39 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
+    public boolean wordPattern(String p, String s) {
         HashMap<Character,String> map=new HashMap<>();
-        HashSet<String> set=new HashSet<>();
-        String[] word=s.split(" ");
-        if(word.length!=pattern.length()){
-            return false;
-        }
-        for(int i=0;i<word.length && i<pattern.length();i++){
-                if(map.containsKey(pattern.charAt(i))){
-                    if(!map.get(pattern.charAt(i)).equals(word[i])){
-                        return false;
-                    }
-                }else{
-                    if(set.contains(word[i])){
-                        return false;
-                    }
-                    map.put(pattern.charAt(i),word[i]);
-                    set.add(word[i]);
+        HashMap<String,Boolean> map2=new HashMap<>();
+        int j=0;
+        for(int i=0;i<p.length();i++){
+            String temp="";
+            for(;j<s.length();j++){
+                
+                if(s.charAt(j)==' '){
+                    j++;
+                    break;
                 }
+                temp+=s.charAt(j);
+                
+            }
+            if(temp==""){
+                return false;
+            }
+            if(map.containsKey(p.charAt(i))){
+                String sp=map.get(p.charAt(i));
+                if(!sp.equals(temp)){
+                    return false;
+                }
+            }else{
+                if(map2.containsKey(temp)){
+                    return false;
+                }else{
+                map.put(p.charAt(i),temp);
+                map2.put(temp,true);
+                }
+            }
+            
+        }
+        if(j<s.length()){
+            return false;
         }
         return true;
     }
